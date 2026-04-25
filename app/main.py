e5a96dc1d0ed12d08936dafbcde81f525d0238af
fbarros12 <fbarros12@gmail.com>
2026-04-25 11:07:45 -0300

    nova versão do projeto

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="Relatório de Motoristas")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
